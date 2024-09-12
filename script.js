2da5cb422aa2eb6441962add70d59a477ee01515
Thedeepakdixit <thedeepakdixit20@gmail.com>
2024-09-12 09:24:37 +0530

    Initial commit for Simple Calculator

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-let currentInput = '';
-let display = document.getElementById('display');
 
-function appendValue(value) {
-    currentInput += value;
-    display.innerText = currentInput;
-}
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-function calculate() {
-    try {
-        currentInput = eval(currentInput).toString();
-        display.innerText = currentInput;
-    } catch (error) {
-        display.innerText = 'Error';
-    }
-}
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
 
-function clearDisplay() {
-    currentInput = '';
-    display.innerText = '0';
-}
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
